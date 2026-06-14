8ca1ab3da31158bd608276f1486fb65b61301388
namdexVND <hoanganhan019@gmail.com>
2026-06-14 22:25:50 +0700

    Update VNDhub.lua

--- VNDhub.lua
+++ VNDhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/namdexVND/VNDhub/refs/heads/main/2869790783551307.lua.txt", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/namdexVND/VNDhub/refs/heads/main/8864525768450670.lua", true))()

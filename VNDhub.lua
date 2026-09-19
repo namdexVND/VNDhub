546b47dd549a5a9c0184aa93a1593a081b7cf202
namdexVND <hoanganhan019@gmail.com>
2026-09-19 13:20:37 +0700

    Update script URL in VNDhub.lua

--- VNDhub.lua
+++ VNDhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/namdexVND/Test/refs/heads/main/obfuscated_script-1787654663763.lua.txt", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/namdexVND/Test/refs/heads/main/5816982256599752.lua", true))()
